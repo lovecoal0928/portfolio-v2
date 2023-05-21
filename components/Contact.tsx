2f0cf1a673c6695856a7471e434ba2b34d998f93
coa3 <lovecoal928@gmail.com>
2023-05-21 22:47:11 +0900

    add フォームデータのfetchとメール送信まで

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,33 @@
-import React from "react"
+import React, { useRef } from "react"
 
 const Contact = () => {
+
+    const nameRef = useRef<HTMLInputElement>(null)
+    const emailRef = useRef<HTMLInputElement>(null)
+    const messageRef = useRef<HTMLTextAreaElement>(null)
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        // console.log(nameRef.current?.value)
+
+        let data = {
+            name: nameRef.current?.value,
+            email: emailRef.current?.value,
+            message: messageRef.current?.value,
+        };
+
+        await fetch("api/contact", {
+            method: 'POST',
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((res) => {
+            if(res.status == 200) console.log("メール送信成功")
+        })
+    }
+
     return (
         <div id='contact' className='bg-red-950 w-full text-[#eee]'>
             <div className='py-6 md:p-10'>
@@ -9,26 +36,28 @@ const Contact = () => {
                     最後までご覧いただきありがとうございました。<br/>
                     もしこのサイトや私について何かコメントがありましたら、下記フォームをご利用ください。</p>
                 <div className="container mt-5">
-                    <form>
+                    <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => handleSubmit(e)}>
                         <div className="mb-3">
                             <label htmlFor="name" className="FormLabel">
                                 お名前
                             </label>
-                            <input type="text" className="FormBox" id="name" required />
+                            <input type="text" className="FormBox" id="name" ref={nameRef} required />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="email" className="FormLabel">
                                 メールアドレス
                             </label>
-                            <input type="email" className="FormBox" id="email" required />
+                            <input type="email" className="FormBox" id="email" ref={emailRef} required />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="message" className="FormLabel">
                                 メッセージ
                             </label>
-                            <textarea name="message" className="FormBox" id="message" required />
+                            <textarea name="message" className="FormBox" id="message" ref={messageRef} required />
                         </div>
-                        <button type="submit" className="SubmitBtn">送信</button>
+                        <button type="submit" className="SubmitBtn">
+                            送信
+                        </button>
                     </form>
                 </div>
             </div>
